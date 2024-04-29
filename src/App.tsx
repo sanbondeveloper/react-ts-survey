43be5582ed39aa92ca5a5b146c9d319e4ea74575
sanbondeveloper <sanbondeveloper@gmail.com>
2024-04-29 23:46:01 +0900

    refactor: App 컴포넌트에서 데이터 가져오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import { useAppDispatch } from './redux/hooks';
+import { changeDescription, changeTitle, initQuestions } from './redux/slices/surveySlice';
 import SurveyPage from './pages/survey';
 
 const router = createBrowserRouter([
@@ -18,6 +21,18 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const title = localStorage.getItem('title') || '';
+    const description = localStorage.getItem('desc') || '';
+    const questions = localStorage.getItem('questions');
+
+    dispatch(changeTitle(title));
+    dispatch(changeDescription(description));
+    dispatch(initQuestions(questions ? JSON.parse(questions) : []));
+  }, [dispatch]);
+
   return (
     <main>
       <RouterProvider router={router} />
